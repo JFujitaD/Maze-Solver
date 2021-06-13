5abc42d317efc0d01dd16d4c528fc606843aa1a5
JFujitaD <fujitajulian@icloud.com>
2021-06-13 12:41:02 -0600

    Created generic method for solving maze

--- src/MazeSolver.java
+++ src/MazeSolver.java
@@ -3,8 +3,14 @@ import java.awt.Point;
 public abstract class MazeSolver {
 	
 	private static MyCell[][] cellMatrix;	// Rows, Columns
+	private static MyCell startCell;
+	private static MyCell endCell;
 	private static int rows;
 	private static int cols;
+	
+	public enum SearchAlgorithm {
+		BREADTH_FIRST, DEPTH_FIRST
+	}
 	
 	public static void setMatrix(MyCell[][] matrix) {
 		cellMatrix = matrix;
@@ -18,5 +24,26 @@ public abstract class MazeSolver {
 	
 	public static void setEndCell(int row, int column) {
 		cellMatrix[row][column].changeToEnd();		
+	}
+	
+	public static boolean solve(SearchAlgorithm algorithm) {
+		if(startCell != null && endCell != null) {
+			switch(algorithm) {
+				case BREADTH_FIRST:
+					breadthFirstSearch();
+					break;
+				case DEPTH_FIRST:
+					depthFirstSearch();
+			}
+			return true;
+		}
+		return false;
+	}
+	
+	public static void breadthFirstSearch() {
+		
+	}
+	public static void depthFirstSearch() {
+		
 	}
 }
